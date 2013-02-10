74c1d55d250a1e71872b68fb8c978e7833ba68a5
Duncan Cross <duncan.cross@gmail.com>
2013-02-10 17:28:59 +0000

    SDL window demo

--- projects/saltstorm/saltstorm.main.lua
+++ projects/saltstorm/saltstorm.main.lua
@@ -1,6 +1,25 @@
 
-local lfs = require 'lfs'
+local sdl = require 'extern.sdl'
+local bit = require 'bit'
+local ffi = require 'ffi'
 
-print('working dir: ' .. lfs.currentdir())
+sdl.SDL_Init(sdl.SDL_INIT_VIDEO)
 
-io.read()
+local VIEWPORT_WIDTH, VIEWPORT_HEIGHT = 320, 200
+
+local sdlVideo = sdl.SDL_SetVideoMode(
+	VIEWPORT_WIDTH, VIEWPORT_HEIGHT,
+	32,
+	bit.bor(sdl.SDL_SWSURFACE, sdl.SDL_RESIZABLE))
+
+local event = ffi.new 'SDL_Event'
+local running = true
+while running do
+  while sdl.SDL_PollEvent(event) do
+    if event.type == sdl.SDL_QUIT then
+	  running = false
+	end
+  end
+end
+
+sdl.SDL_Quit()
